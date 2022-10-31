c1013beccc2dd4b910465174e59b0c00bf0ae90b
mesbahpour <mesbahpour.a@gmail.com>
2022-10-31 04:14:40 +0330

    change loader style

--- js/script.js
+++ js/script.js
@@ -1,60 +1,51 @@
-$(document).ready(function(){
-	$(window).resize(function(){
-		var w=$(window).width();
-		var h=$(window).height();
-		$(".viewports").html(w+" * "+h);
-	})
+$(document).ready(function () {
+  $(window).resize(function () {
+    var w = $(window).width();
+    var h = $(window).height();
+    $(".viewports").html(w + " * " + h);
+  });
 
+  $(".gotoTop").fadeOut(0);
 
-	$('.gotoTop').fadeOut(0);
-
-	$(window).scroll(function(){
-		var topscroll=$(window).scrollTop();
-		// $(".viewports").html(topscroll);
-		if(topscroll>70){
-			$("nav").addClass("fixed");
-
-		}
-		else{
-			$("nav").removeClass("fixed");	
-		}
-
-		if(topscroll>150){
-			$(".gotoTop").fadeIn(500);
-		}
-		else{
-			$(".gotoTop").fadeOut(500);
-		}
-
-	})
-	$('.gotoTop').click(function(){
-		$('html,body').animate({scrollTop:0},'2000');
-	})
-
-
-
-})
-
-$(function() { var logo = $(".lrg-logo"); $(window).scroll(function() {
-var scroll = $(window).scrollTop();
-
-    if (scroll >= 100) {
-      if(!logo.hasClass("sml-logo")) {
-        logo.hide();
-        logo.removeClass('lrg-logo').addClass("sml-logo").fadeIn( "fast");
-      }
+  $(window).scroll(function () {
+    var topscroll = $(window).scrollTop();
+    // $(".viewports").html(topscroll);
+    if (topscroll > 70) {
+      $("nav").addClass("fixed");
     } else {
-      if(!logo.hasClass("lrg-logo")) {
-        logo.hide();
-        logo.removeClass("sml-logo").addClass('lrg-logo').fadeIn( "fast");
-      }
+      $("nav").removeClass("fixed");
     }
 
-});
-});
-
-
-
-window.addEventListener("load", function(){
-	document.getElementById("loader-container").style.display = "none";
+    if (topscroll > 150) {
+      $(".gotoTop").fadeIn(500);
+    } else {
+      $(".gotoTop").fadeOut(500);
+    }
   });
+  $(".gotoTop").click(function () {
+    $("html,body").animate({ scrollTop: 0 }, "2000");
+  });
+});
+
+$(function () {
+  var logo = $(".lrg-logo");
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+      if (!logo.hasClass("sml-logo")) {
+        logo.hide();
+        logo.removeClass("lrg-logo").addClass("sml-logo").fadeIn("fast");
+      }
+    } else {
+      if (!logo.hasClass("lrg-logo")) {
+        logo.hide();
+        logo.removeClass("sml-logo").addClass("lrg-logo").fadeIn("fast");
+      }
+    }
+  });
+});
+
+window.addEventListener("load", function () {
+  document.getElementById("loader-container").style.display = "none";
+});
